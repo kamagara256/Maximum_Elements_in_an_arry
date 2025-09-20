996f2c7daad3bab9a2eeb3a43d3579f6d299a03a
kamagara256 <albertkamagara1@gmail.com>
2025-09-20 15:45:35 +0300

    Add files via upload

---
+++ max-element.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    // Input array size
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; // Declare array
+
+    // Input array elements
+    printf("Enter %d elements separated by spaces:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Assume first element is maximum
+    int max = arr[0];
+
+    // Compare with the rest
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    // Output result
+    printf("Maximum element: %d\n", max);
+
+    return 0;
+}
